b81b087319ef48505bc2142c0563d2bbce69f724
Henry Garcia <hgarcia8889@gmail.com>
2025-05-19 15:07:39 -0400

    Fifth commit.

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ function checkLightMode() {
         lightEl.id = "lightmode";
         lightEl.style = "color: #1f1f1f";
         h1El.textContent = "Light Mode";
-        buttonEl.textContent = "Enable Dark Mode";
+        lightEl.lastChild.textContent = "Enable Dark Mode";
     } else {
         lightEl.id = "darkmode";
         lightEl.style ="color: #f0f1f5";
         h1El.textContent = "Dark Mode";
-        buttonEl.textContent = "Disable Dark Mode";
+        lightEl.lastChild.textContent = "Disable Dark Mode";
     }    
 } 
 
