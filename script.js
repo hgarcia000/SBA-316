1858cf3f8960f43a020e7ae5fc548f887d4dc2ad
Henry Garcia <hgarcia8889@gmail.com>
2025-05-19 14:35:39 -0400

    Fourth commit.

--- script.js
+++ script.js
@@ -3,6 +3,25 @@ const lightEl = document.getElementById("lightmode");
 const h1El = document.querySelector("h1");
 
 const buttonEl = document.createElement("button");
-buttonEl.textContent = "Toggle Dark Mode";
+buttonEl.textContent = "Enable Dark Mode";
 lightEl.appendChild(buttonEl);
 
+let isLightMode = true;
+function checkLightMode() {
+    if (isLightMode) {
+        lightEl.id = "lightmode";
+        lightEl.style = "color: #1f1f1f";
+        h1El.textContent = "Light Mode";
+        buttonEl.textContent = "Enable Dark Mode";
+    } else {
+        lightEl.id = "darkmode";
+        lightEl.style ="color: #f0f1f5";
+        h1El.textContent = "Dark Mode";
+        buttonEl.textContent = "Disable Dark Mode";
+    }    
+} 
+
+buttonEl.addEventListener("click", () =>{
+    isLightMode = !isLightMode;
+    checkLightMode();
+});
