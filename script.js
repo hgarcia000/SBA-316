c7071915192df49f78209d38239b7bf85ad1a9df
Henry Garcia <hgarcia8889@gmail.com>
2025-05-19 16:24:22 -0400

    Added form validation.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const h1El = document.querySelector("h1");
 
 const buttonEl = document.createElement("button");
 buttonEl.textContent = "Enable Dark Mode";
-lightEl.appendChild(buttonEl);
+lightEl.prepend(buttonEl);
 
 let isLightMode = true;
 function checkLightMode() {
@@ -12,12 +12,12 @@ function checkLightMode() {
         lightEl.id = "lightmode";
         lightEl.style = "color: #1f1f1f";
         h1El.textContent = "Light Mode";
-        lightEl.lastChild.textContent = "Enable Dark Mode";
+        buttonEl.textContent = "Enable Dark Mode";
     } else {
         lightEl.id = "darkmode";
         lightEl.style ="color: #f0f1f5";
         h1El.textContent = "Dark Mode";
-        lightEl.lastChild.textContent = "Disable Dark Mode";
+        buttonEl.textContent = "Disable Dark Mode";
     }    
 } 
 
@@ -25,3 +25,29 @@ buttonEl.addEventListener("click", () =>{
     isLightMode = !isLightMode;
     checkLightMode();
 });
+
+const responseEl = document.createElement("h3");
+
+const formEl = document.querySelector("form");
+const ageEl = formEl.elements["age"];
+
+
+formEl.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const ageInvalid = validateAge(e.target.elements["age"].value);
+    if (ageInvalid) {
+        alert("Invalid age!");
+    } else {
+        const confirmation = confirm("Would you like to submit this response?");
+        if (confirmation) {
+            responseEl.textContent = `Hello, ${e.target.elements["name"].value}! I see you're ${e.target.elements["age"].value} years old!`;
+            lightEl.appendChild(responseEl);
+        }
+        
+    }
+        
+});
+
+function validateAge(e){
+    return e < 1;
+}
