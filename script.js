154c4fb68c479ed0ceedc6a0886c19e1ec9a97e1
Henry Garcia <hgarcia8889@gmail.com>
2025-05-19 14:11:01 -0400

    Third commit.

---
+++ script.js
@@ -0,0 +1,8 @@
+const lightEl = document.getElementById("lightmode");
+
+const h1El = document.querySelector("h1");
+
+const buttonEl = document.createElement("button");
+buttonEl.textContent = "Toggle Dark Mode";
+lightEl.appendChild(buttonEl);
+
