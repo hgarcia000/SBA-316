4408ba8cb53a423af2cc7ac90bd66b9d8371893e
Henry Garcia <hgarcia8889@gmail.com>
2025-05-21 15:43:39 -0400

    Adjusted styling.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const lightEl = document.getElementById("lightmode");
 
 const h1El = document.querySelector("h1");
 
+document.body.style = "background-color: #f0f1f5";
+
 const buttonEl = document.createElement("button");
 buttonEl.textContent = "Enable Dark Mode";
 lightEl.prepend(buttonEl);
@@ -10,11 +12,13 @@ let isLightMode = true;
 function checkLightMode() {
     if (isLightMode) {
         lightEl.id = "lightmode";
+        document.body.style = "background-color: #f0f1f5";
         lightEl.style = "color: #1f1f1f";
         h1El.textContent = "Light Mode";
         lightEl.firstChild.textContent = "Enable Dark Mode";
     } else {
         lightEl.id = "darkmode";
+        document.body.style = "background-color: #1f1f1f";
         lightEl.style ="color: #f0f1f5";
         h1El.textContent = "Dark Mode";
         lightEl.firstChild.textContent = "Disable Dark Mode";
